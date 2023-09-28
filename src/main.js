af7b94d070b559945bb55caf1e6aaec560bd1c99
Guilherme Fujii <gui.fujii@outlook.com>
2023-09-28 15:50:24 -0300

    fix: cors

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // Application modules
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT || 3000
